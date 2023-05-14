6f2361297656dadc28ac01fe1386564359724d29
Mikasa665 <133528192+Mikasa665@users.noreply.github.com>
2023-05-14 15:09:53 +0300

    Add files via upload

---
+++ plugins/game-acertijo.js
@@ -0,0 +1,37 @@
+import fs from 'fs'
+
+let timeout = 60000
+let poin = 500
+
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tekateki = conn.tekateki ? conn.tekateki : {}
+    let id = m.chat
+    if (id in conn.tekateki) {
+        conn.reply(m.chat, 'لا تزال هناك ألغاز لم تتم الإجابة عليها في هذه الدردشة ', conn.tekateki[id][0])
+        throw false
+    }
+    let tekateki = JSON.parse(fs.readFileSync(`./src/game/acertijo.json`))
+    let json = tekateki[Math.floor(Math.random() * tekateki.length)]
+    let _clue = json.جواب
+    let clue = _clue.replace(/[A-Za-z]/g, '_')
+    let caption = `
+ⷮ *${json.سوال}*
+
+*• الوقت:* ${(timeout / 1000).toFixed(2)} ثانيه
+*• بونو:*كسبت +${poin} Exp
+`.trim()
+    conn.tekateki[id] = [
+       await conn.reply(m.chat, caption, m),
+        json, poin,
+        setTimeout(async () => {
+            if (conn.tekateki[id]) await conn.reply(m.chat, `انتهى الوقت !\n*جواب:* ${json.جواب}`, conn.tekateki[id][0])
+            delete conn.tekateki[id]
+        }, timeout)
+    ]
+}
+
+handler.help = ['acertijo']
+handler.tags = ['game']
+handler.command = /^(acertijo|سؤال|pregunta|adivinanza|tekateki)$/i
+
+export default handler
---
+++ plugins/game-sort.js
@@ -0,0 +1,36 @@
+import fs from 'fs'
+
+let timeout = 15000
+let poin = 500
+
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tekateki = conn.tekateki ? conn.tekateki : {}
+    let id = m.chat
+    if (id in conn.tekateki) {
+        conn.reply(m.chat, 'لا تزال هناك ألغاز لم تتم الإجابة عليها في هذه الدردشة ', conn.tekateki[id][0])
+        throw false
+    }
+    let tekateki = JSON.parse(fs.readFileSync(`./src/game/sort.json`))
+    let json = tekateki[Math.floor(Math.random() * tekateki.length)]
+    let _clue = json.جواب
+    let clue = _clue.replace(/[A-Za-z]/g, '_')
+    let caption = `
+ⷮ *${json.سوال}*
+
+*• الوقت:* ${(timeout / 1000).toFixed(2)} ثانيه
+`.trim()
+    conn.tekateki[id] = [
+       await conn.reply(m.chat, caption, m),
+        json, poin,
+        setTimeout(async () => {
+            if (conn.tekateki[id]) await conn.reply(m.chat, `انتهى الوقت !\n*جواب:* ${json.جواب}`, conn.tekateki[id][0])
+            delete conn.tekateki[id]
+        }, timeout)
+    ]
+}
+
+handler.help = ['acertijo']
+handler.tags = ['game']
+handler.command = /^(رتب|تر)$/i
+
+export default handler
